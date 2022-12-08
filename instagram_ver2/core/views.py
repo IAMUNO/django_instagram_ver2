6341556f55326aa534928198f11dca8e6ba3cf08
Yoo Yunho <yyh3690@naver.com>
2022-12-08 18:31:03 +0900

    signup views 작성

--- instagram_ver2/core/views.py
+++ instagram_ver2/core/views.py
@@ -1,5 +1,8 @@
+from .models import Profile
+from django.contrib import messages
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 
 
 def index(request):
@@ -7,4 +10,34 @@ def index(request):
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'EMAIL ALREADY TAKEN')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'USERNAME ALREADY TAKEN')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                # log user in and redirect to settings page
+
+                # create profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('login')
+
+        else:
+            messages.info(request, 'PASSWORD NOT MATCHING')
+            return redirect('signup')
+
+    else:
+        return render(request, 'signup.html')
