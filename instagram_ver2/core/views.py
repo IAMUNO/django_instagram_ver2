f18135592f96f1713d5b89b1c9c761a06607d57a
Yoo Yunho <yyh3690@naver.com>
2022-12-08 20:10:03 +0900

    settings register

--- instagram_ver2/core/views.py
+++ instagram_ver2/core/views.py
@@ -1,12 +1,42 @@
 from .models import Profile
 from django.contrib import messages
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
 
 
+@login_required(login_url="signin")
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required(login_url="signin")
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == "POST":
+        if request.FILES.get('image') is None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') is not None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        return redirect('settings')
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 
 def signup(request):
@@ -28,12 +58,15 @@ def signup(request):
                 user.save()
 
                 # log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
 
                 # create profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signin')
+                return redirect('settings')
 
         else:
             messages.info(request, 'PASSWORD NOT MATCHING')
@@ -61,6 +94,7 @@ def signin(request):
         return render(request, 'signin.html')
 
 
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
