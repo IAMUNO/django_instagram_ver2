8f0657ae9b365a1d59678521bd872095dd2ee13f
Yoo Yunho <yyh3690@naver.com>
2022-12-09 15:08:09 +0900

    views upload 수정

--- instagram_ver2/core/views.py
+++ instagram_ver2/core/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 
-from .models import Profile
+from .models import Profile, Post
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
@@ -12,6 +12,22 @@ def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
     return render(request, 'index.html', {'user_profile': user_profile})
+
+
+@login_required(login_url='signin')
+def upload(request):
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+
+    else:
+        return redirect('/')
 
 
 @login_required(login_url="signin")
@@ -103,6 +119,4 @@ def logout(request):
     return redirect('signin')
 
 
-@login_required(login_url='signin')
-def upload(request):
-    return HttpResponse('<h1>Upload View</h1>')
+
